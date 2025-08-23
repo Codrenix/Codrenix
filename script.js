2e7b16a6a23300863824b0ae74997f5828e5fbc0
Codrenix <codrenix@gmail.com>
2025-08-23 18:35:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -36,7 +36,8 @@ Name: ${name}
 Email: ${email}
 Subject: ${subject}
 
-Message:${message}
+Message:
+${message}`;
 
   const url = `https://wa.me/${919552477146}?text=${encodeURIComponent(text)}`;
   window.open(url, "_blank");
